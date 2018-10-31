ab1b8f7896dee20a1a6f0f9afef709b217be9e60
Kenneth Wilkinson <kwilkinson1@ufl.edu>
2018-10-30 21:13:09 -0400

    First commit

---
+++ server/express.js
@@ -0,0 +1,33 @@
+var path = require('path'),
+    express = require('express'),
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    config = require('./config'),
+    listingsRouter = require('../routes/listings.server.routes'),
+    getCoordinates = require('../controllers/coordinates.server.controller.js');
+
+module.exports.init = function() {
+  //connect to database
+  mongoose.connect(config.db.uri);
+
+  //initialize app
+  var app = express();
+
+  //enable request logging for development debugging
+  app.use(morgan('dev'));
+
+  //body parsing middleware
+  app.use(bodyParser.json());
+
+/*Server connection to Amazon S3
+
+  /* serve static files */
+
+  /* go to homepage for all routes not specified */
+  app.get('*', function(req, res){
+    res.redirect('/');
+  });
+
+  return app;
+};
