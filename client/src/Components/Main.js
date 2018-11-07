d0dd28288314221feebfb3e754f76f3215411aad
clange9530 <clange9530@gmail.com>
2018-11-07 11:36:05 -0500

    Reorganized front end files for better organization.

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Users from './Pages/Users';
-import Home from './Pages/Home';
-import Teams from './Pages/Teams';
+import Users from '../Pages/Users';
+import Home from '../Pages/Home';
+import Teams from '../Pages/Teams';
 
 
 
