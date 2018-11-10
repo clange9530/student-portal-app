8fb850e18cbee473578dfb3937e032bece0a533a
Kenneth Wilkinson <kwilkinson1@ufl.edu>
2018-11-09 20:26:08 -0500

    Updated group schema

--- server/models/teamModel.js
+++ server/models/teamModel.js
@@ -1,1 +1,18 @@
 //Create schema for team profiles
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var teamSchema = new Schema({
+     members: [],  //array for team members
+     projects: [],  //array for projects assigned
+     skills: [],  //array for skills of team members
+     prod_mgr: String,  //This is for the product manager
+     scrum_mstr: String,  //This is for the name of the scrum master
+     primary_cont:  String  //This is for the primary contact name
+
+});
+
+var Team = mongoose.model('Team', teamSchema);
+
+/* Export the model to make it avaiable to other parts of your Node application */
+module.exports = Team;
