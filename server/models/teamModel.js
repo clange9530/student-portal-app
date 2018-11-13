aa10258e56e21f9c62c2df0b8a5ef3639e07161d
Kenneth Wilkinson <kwilkinson1@ufl.edu>
2018-11-12 21:30:46 -0500

    Modified files dealing with team structure
    Added HTML to client/src/Pages/Teams.js to reflect style of both
    individual page and wire frame diagram
    
    Changed coding style of teamModel.js schema to reflect that of other
    schema

--- server/models/teamModel.js
+++ server/models/teamModel.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var teamSchema = new Schema({
-     members: [],  //array for team members
-     projects: [],  //array for projects assigned
-     skills: [],  //array for skills of team members
+     members: Array,  //array for team members
+     projects: Array,  //array for projects assigned
+     skills: Array,  //array for skills of team members
      prod_mgr: String,  //This is for the product manager
      scrum_mstr: String,  //This is for the name of the scrum master
      primary_cont:  String  //This is for the primary contact name
