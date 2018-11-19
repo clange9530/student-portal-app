1aacef05af55a061d4ab356e466bb108e0deea24
clange9530 <clange9530@gmail.com>
2018-11-18 20:48:06 -0500

    Fixing merges 2.0

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -8,15 +8,11 @@ const Header = () => (
         <li class="navBar"><Link to='/'   >Home</Link></li>
         <li class="navBar"><Link to='/users'>Individual Profiles</Link></li>
         <li class="navBar"><Link to='/teams'>Teams</Link></li>
-<<<<<<< HEAD
-        <li class="navBar"><Link to='/sendMail'>Send Mail</Link></li>
         <li class="navBar"><Link to="/createProfile">Create New Profile</Link></li>
-=======
         <li class="navBar"><Link to='/sendMail/566372f4d11de3498e2941c9'>Send Mail</Link></li>
         <li class="navBar"><Link to="/listmail/566372f4d11de3498e2941c9">View emails</Link></li>    {/* TODO: Make project id dynamic */}
         <li class="navBar"><Link to="/studentsurvey/566372f4d11de3498e2941c9">Submit survey</Link></li>    {/* TODO: Make project id dynamic */}
         <li class="navBar"><Link to="/clientsurvey/566372f4d11de3498e2941c9">View client survey</Link></li>     {/* TODO: Make project id dynamic */}
->>>>>>> master
       </ul>
     </nav>
   </header>
--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -3,15 +3,11 @@ import { Switch, Route } from 'react-router-dom';
 import Users from '../Pages/Users';
 import Home from '../Pages/Home';
 import Teams from '../Pages/Teams';
-<<<<<<< HEAD
-import sendMail from '../Components/SendMail';
 import CreateProfile from '../Pages/CreateProfile';
-=======
 import SendMail from './SendMail';
 import ListMail from './ListMail';
 import StudentSurvey from './StudentSurvey';
 import ClientSurvey from './ClientSurvey';
->>>>>>> master
 
 const Main = () => (
     <main>
@@ -19,15 +15,11 @@ const Main = () => (
             <Route exact path='/' component={Home}/>
             <Route path='/teams' component={Teams}/>
             <Route path='/users' component={Users}/>
-<<<<<<< HEAD
-            <Route path='/sendMail' component={sendMail}/>
             <Route path='/createProfile' component={CreateProfile}/>
-=======
             <Route path="/sendmail/:projectId/:emailId?" component={SendMail} />
             <Route path="/listmail/:projectId" component={ListMail} />
             <Route path="/studentsurvey/:projectId" component={StudentSurvey} />
             <Route path="/clientsurvey/:projectId" component={ClientSurvey} />
->>>>>>> master
         </Switch>
     </main>
 )
