b0e1df75726e017843cefe6f6f1780d26018cf36
mrmvy <42630747+mrmvy@users.noreply.github.com>
2018-12-12 20:05:13 -0600

    Delete awsConfig.js

--- client/src/awsConfig.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-     aws:{
-          AWS_BUCKET_NAME: 'cen-3031-student-portal-app',
-          AWS_ACCESS_KEY: 'AKIAJ7GSJ5F3MEYEP5FQ',
-          AWS_SECRET_KEY: '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',
-          AWS_REGION: 'us-east-1'
-     }
-}
